f1f51e5773e80001328730af10157f0fdde41b94
flori <floritrs@gmail.com>
2024-12-03 07:27:44 +0100

    Add time benchmarks to template

--- 2024/src/bin/d0x.rs
+++ 2024/src/bin/d0x.rs
@@ -1,18 +1,35 @@
 use std::{fs::File, io::Read};
-
+use std::time::Instant;
 
 
 fn main(){
+    // get day number from file
     let day = file!().split('/').last().unwrap()[1..3].to_owned();
 
+    // read input
     let path = format!("src/input/input{day}.txt");
     println!("{}", path);
     let mut file = File::open(path).expect("File not found");
     let mut input = String::new();
     file.read_to_string(&mut input).expect("cannot read file");
 
-    println!("{}", p1(&input));
-    println!("{}", p2(&input));
+    // measure time needed to run both parts
+    let mut now: Instant;
+    let mut elapsed: std::time::Duration;
+
+    now = Instant::now();
+    let p1result = p1(&input);
+    elapsed = now.elapsed();
+
+    println!("Part 1 solution in: {:.2?}", elapsed);
+    println!("{}", p1result);
+
+    now = Instant::now();
+    let p2result = p2(&input);
+    elapsed = now.elapsed();
+
+    println!("Part 2 solution in: {:.2?}", elapsed);
+    println!("{}", p2result);
 }
 
 
@@ -36,18 +53,14 @@ mod test {
     #[test]
     fn test_part1() {
         let expected = 0;
-        let example = r"
-
-        ";
+        let example = r"";
         assert_eq!(p1(example), expected);
     }
 
     #[test]
     fn test_part2() {
         let expected = 0;
-        let example = r"
-
-        ";
+        let example = r"";
         assert_eq!(p2(example), expected);
     }
 }
